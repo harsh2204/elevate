e30df0ab25847eace4fc7fcf96bef1811d31ee5b
MahiFaiyaz <68677484+MahiFaiyaz@users.noreply.github.com>
2022-11-20 01:28:40 -0500

    Changed to chartjs

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
 function loadBarGraph() {
-    chart.options.data[0].dataPoints = [];
+  chart.data.datasets[0].data = [];
   var e = document.getElementById("dd");
-    var selected = e.options[e.selectedIndex].value;
+  var selected = e.options[e.selectedIndex].value;
   var dps = jsonData[selected];
   for(var i in dps) {
-      var xVal = dps[i].x;
-    chart.options.data[0].dataPoints.push({label: xVal, y: dps[i].y});
+    chart.data.datasets[0].data.push(dps[i]);
   }
-  chart.render();
+  chart.update();
 }
 
-
 var jsonData = {
-    "dt": [
-        { "x": "Saturday", "y": 2 },
-        { "x": "Sunday", "y": 6 },
-        { "x": "Monday", "y": 3 },
-        { "x": "Tuesady", "y": 2 },
-        { "x": "Wednesday", "y": 5 },
-        { "x": "Thursday", "y": 4 },
-        { "x": "Friday", "y": 7 },
-    ],
-    "wt": [
-        { "x": "Saturday", "y": 10000 },
-        { "x": "Sunday", "y": 11000 },
-        { "x": "Monday", "y": 10400 },
-        { "x": "Tuesady", "y": 12000 },
-        { "x": "Wednesday", "y": 8000 },
-        { "x": "Thursday", "y": 9500 },
-        { "x": "Friday", "y": 10800 },
-    ]}
-    var dataPoints = [];
-    var chart = new CanvasJS.Chart("chartContainer",
-    {
-        data: [{
-        type: 'column',
-        showInLegend: true,
-        legendText: "Day of the week",
-        dataPoints: dataPoints // this should contain only specific serial number data
-        }]
-    });
-    
+  "dt": [2, 6, 3 ,2 ,5 , 4 ,7],
+  "wt": [ 10000, 11000, 10400, 12000, 8000, 9500, 10800]
+}
+
+var dataPoints = [];
+
+const ctx = document.getElementById('chartContainer');
+  
+var chart = new Chart(ctx,{
+  type: 'bar',
+  data: {
+    labels: ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    datasets: [{
+      label: '',
+      data: [],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  }
+});
+  
 $( ".dropdown" ).change(loadBarGraph);
 
 $(document).ready(loadBarGraph)
